db6a439186f2e01314a99d29f6169bc1f842fc4c
Hosseini <asgharhosseini.v@gmail.com>
2022-03-10 07:17:35 +0330

    create base app

--- app/src/main/java/ir/ah/igapweather/MainActivity.kt
+++ app/src/main/java/ir/ah/igapweather/MainActivity.kt
@@ -1,11 +1,34 @@
 package ir.ah.igapweather
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.isVisible
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupWithNavController
+import dagger.hilt.android.AndroidEntryPoint
+import ir.ah.igapweather.databinding.ActivityMainBinding
 
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration)
+    }
+
 }
---
+++ app/src/main/java/ir/ah/igapweather/di/AuthorizationHeaderInterceptor.kt
@@ -0,0 +1,15 @@
+package ir.ah.igapweather.di
+import okhttp3.Interceptor
+import okhttp3.Response
+import javax.inject.Inject
+
+class AuthorizationHeaderInterceptor @Inject constructor(
+
+) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest = chain.request().newBuilder()
+
+        val newRequest = originalRequest.build()
+        return chain.proceed(newRequest)
+    }
+}
